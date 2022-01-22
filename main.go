8fb0502284575f86ab896435ad9c78ae161df9ec
xdahiel <2018856302@qq.com>
2022-01-22 15:53:49 +0800

    test: push with ubuntu

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "welcome to chun oj")
+	_, err := fmt.Fprintln(w, "welcome to chun oj")
+	if err != nil {
+		return
+	}
 }
 
 func main() {
 	server := http.Server{
-		Addr: "127.0.0.1:8000",
-		Handler : nil,
+		Addr:    "127.0.0.1:8000",
+		Handler: nil,
 	}
 	http.HandleFunc("/", welcome)
 	server.ListenAndServe()
