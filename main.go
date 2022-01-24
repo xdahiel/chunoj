c62ad8e3e0d0cb6d1a58399f2ebf3e2b28e68646
xdahiel <2018856302@qq.com>
2022-01-24 13:47:44 +0800

    test: template with action

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 )
 
-func welcome(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintln(w, "welcome to chun oj")
-	if err != nil {
-		return
-	}
+func formatDate(t time.Time) string {
+	layout := "2006-01-02"
+	return t.Format(layout)
 }
-
+func process(w http.ResponseWriter, r *http.Request) {
+	funcMap := template.FuncMap{"fdate": formatDate}
+	t := template.New("templates/tmpl.html").Funcs(funcMap)
+	t, _ = t.ParseFiles("templates/tmpl.html")
+	t.Execute(w, time.Now())
+}
 func main() {
 	server := http.Server{
-		Addr:    "127.0.0.1:8000",
-		Handler: nil,
+		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/", welcome)
+	http.HandleFunc("/process", process)
 	server.ListenAndServe()
 }
